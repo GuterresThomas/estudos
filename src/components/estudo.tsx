21e08f0e4b2490e70be610bba9d24988bf2df1e3
Guterresthomas <guterresmikael@gmail.com>
2023-07-19 21:30:00 -0300

    zod

--- src/components/estudo.tsx
+++ src/components/estudo.tsx
@@ -1,46 +1,86 @@
+'use client'
 import React from 'react'
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
-interface FormProps {
-  onSubmit: (data: FormData) => void;
+
+const FormSchema = z.object({
+    username: z
+    .string()
+    .min(4, {message:"the username must be 4 letters or more"})
+    .max(10, { message: "The username must be 10 characters or less" })
+    .regex(
+      /^[a-zA-Z0-9_]+$/,
+      "The username must contain only letters, numbers and underscore (_)"
+    ),
+    email: z.string().email({message:"Invalid email, please enter a valid email"}),
+    isAdmin: z.boolean(),
+    createdAt: z.coerce.date()
+    .min(new Date(1920, 0, 1), {
+        message: "Date cannot go past January 1 1920",
+      })
+      .max(new Date(), { message: "Date must be in the past" })
+      .refine(
+        (date) => {
+          const ageDifMs = Date.now() - date.getTime();
+          const ageDate = new Date(ageDifMs);
+  
+          const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+  
+          return age >= 18;
+        },
+        { message: "You must be 18 years or older" }
+      ),
+});
+
+type FormInput = z.infer<typeof FormSchema>;
+
+export default function Form() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormInput>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            username: '',
+            email: '',
+            isAdmin: true,
+            createdAt: new Date(),
+        },
+    });
+
+    return (
+        <div className='justfify-center flex bg-zinc-500 rounded-md p-2'>
+            <form onSubmit={handleSubmit((d) => console.log(d))} className=' font-sans-Roboto text-lg font-semibold space-y-2  '>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input id="username" {...register('username')} className='bg-zinc-100 rounded-md mt-2 ml-2'/>
+                {errors?.username?.message && <p className=' text-red-800 font-bolder text-sm'>{errors.username.message}</p>}
+            </div>
+
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input id="email" {...register('email')} className='bg-zinc-100 rounded-md mt-2 ml-12'/>
+                {errors?.email?.message && <p  className=' text-red-800 font-bolder text-sm'>{errors.email.message}</p>}
+            </div>
+
+            <div>
+                <label htmlFor="isAdmin">IsAdmin</label>
+                <input id="isAdmin" type="checkbox" {...register('isAdmin')} className='bg-zinc-100 rounded-md mt-2 ml-2' />
+            </div>
+
+            <div>
+                <label htmlFor="createdAt">Creation Date</label>
+                <input id="createdAt" type="date" {...register('createdAt')} className='bg-zinc-100 rounded-md mt-2 ml-2' />
+                {errors?.createdAt?.message && (
+                    <p className=' text-red-800 font-bolder text-sm'>{errors.createdAt.message}</p>
+                )}
+            </div>
+
+            <button type="submit" className='bg-zinc-800 text-zinc-50 hover:bg-zinc-600 rounded-md p-2'>Submit</button>
+        </form>
+        </div>
+    );
 }
-
-interface FormData {
-  name: string;
-  age: number;
-  email: string;
-}
-
-function Form({ onSubmit }: FormProps) {
-  const [formData, setFormData] = React.useState<FormData>({ name: '', age: 0, email: ''});
-
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  }
-
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    onSubmit(formData);
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
-      </label>
-      <br />
-      <label>
-        Age:
-        <input type="number" name="age" value={formData.age} onChange={handleInputChange} />
-      </label>
-      <br />
-      <label>
-        <input type="text" name='email' value={formData.email} onChange={handleInputChange} />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
-
-export default Form;
