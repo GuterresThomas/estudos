9f42b35f36aec8925b0f2ac2c6b13dbc42eb8e50
Guterresthomas <guterresmikael@gmail.com>
2023-08-09 17:37:34 -0300

    fg

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,10 +2,12 @@
 import React from "react";
 import ProductsApp from "@/components/products"
 import UserRouteProtection from "@/components/userRouteProtection"
+import Link from "next/link"
 
 const testeDb = () => {
     return (
     <UserRouteProtection>
+        <Link href="/user" className="font-semibold">Voltar</Link>
         <ProductsApp />
     </UserRouteProtection>
     )
--- src/components/products.tsx
+++ src/components/products.tsx
@@ -3,76 +3,97 @@
 
 import React, { useState, useEffect } from 'react';
 
-const ProductsApp = () => {
-  const [products, setProducts] = useState([]);
-  const [newProduct, setNewProduct] = useState({ name: '', price: 0 });
+const StoragesApp = () => {
+  const [storages, setStorages] = useState([]);
+  const [newStorage, setNewStorage] = useState({ name: '', price: 0, quantity: 0 });
 
-  const fetchProducts = async () => {
-    const response = await fetch('http://localhost:3000/products');
+  const fetchStorages = async () => {
+    const response = await fetch('http://localhost:3000/storages');
     const data = await response.json();
-    setProducts(data);
+    setStorages(data);
   };
 
-  const addProduct = async () => {
-    const response = await fetch('http://localhost:3000/products', {
+  const addStorage = async () => {
+    const response = await fetch('http://localhost:3000/storages', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(newProduct),
+      body: JSON.stringify(newStorage),
     });
     if (response.status === 201) {
-      setNewProduct({ name: '', price: 0 });
-      fetchProducts();
+      setNewStorage({ name: '', price: 0, quantity: 0 });
+      fetchStorages();
     }
   };
 
-  const deleteProduct = async (id) => {
-    const response = await fetch(`http://localhost:3000/products/${id}`, {
+  const deleteStorage = async (id) => {
+    const response = await fetch(`http://localhost:3000/storages/${id}`, {
       method: 'DELETE',
     });
     if (response.status === 204) {
-      fetchProducts();
+      fetchStorages();
     }
   };
 
   useEffect(() => {
-    fetchProducts();
+    fetchStorages();
   }, []);
 
   return (
-    <div className="flex justify-center mt-10 font-sans-Roboto ">
-        <div className="">
-            <h1 className="font-bold">Lista de Produtos</h1>
-            <ul className="space-y-2  m-4 p-4 ">
-                {products.map((product) => (
-                <li key={product.id}>
-                    {product.name} - R${product.price.toFixed(2)}
-                    <button className="bg-zinc-500 p-1 m-1 rounded-full font-bold text-sm text-zinc-50 hover:bg-zinc-800 " onClick={() => deleteProduct(product.id)}>Excluir</button>
-                </li>
-                ))}
-            </ul>
-            <h2 className="font-bold">Adicionar Novo Produto</h2>
-            <input
-                className="m-2 bg-zinc-300 p-1 rounded-2xl"
-                type="text"
-                placeholder="Nome"
-                value={newProduct.name}
-                onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
-            />
-            <input
+    <div className="flex justify-center mt-10 font-sans-Roboto">
+      <div className="">
+        <h1 className="font-bold">Lista de Estoque</h1>
+        <ul className="space-y-2 m-4 p-4">
+          {storages.map((storage) => (
+            <li key={storage.id}>
+              {storage.name} - R${parseFloat(storage.price).toFixed(2)} - Quantidade: {storage.quantity}
+              <button className="bg-zinc-500 p-1 m-1 rounded-full font-bold text-sm text-zinc-50 hover:bg-zinc-800 " onClick={() => deleteStorage(storage.id)}>Excluir</button>
+            </li>
+          ))}
+        </ul>
+        <h2 className="font-bold">Adicionar Novo Produto ao Estoque:</h2>
+        <div className="flex justify-center">
+            <label htmlFor="Nome">
+                <p className="text-sm font-sans-Roboto font-semibold ml-3">Nome:</p>
+                <input
+                  className="m-2 bg-zinc-300 p-1 rounded-2xl"
+                  type="text"
+                  placeholder="Nome"
+                  value={newStorage.name}
+                  onChange={(e) => setNewStorage({ ...newStorage, name: e.target.value })}
+                />
+            </label>
+            <label htmlFor="Nome">
+              <p className="text-sm font-sans-Roboto font-semibold ml-3">Preço:</p>  
+              <input
                 className="m-2 bg-zinc-300 p-1 rounded-2xl"
                 type="number"
                 placeholder="Preço"
-                value={newProduct.price}
+                value={newStorage.price}
                 onChange={(e) =>
-                setNewProduct({ ...newProduct, price: parseFloat(e.target.value) })
+                  setNewStorage({ ...newStorage, price: parseFloat(e.target.value) })
                 }
-            />
-            <button className="bg-zinc-500 p-1 m-1 rounded-full font-bold text-sm text-zinc-50 hover:bg-zinc-800 " onClick={addProduct}>Adicionar</button>
+              />
+            </label>
+            <label htmlFor="Nome">
+              <p className="text-sm font-sans-Roboto font-semibold ml-3">Quantidade:</p>  
+              <input
+                className="m-2 bg-zinc-300 p-1 rounded-2xl"
+                type="number"
+                placeholder="Quantidade"
+                value={newStorage.quantity}
+                onChange={(e) =>
+                  setNewStorage({ ...newStorage, quantity: parseInt(e.target.value) })
+                }
+              />
+            </label>
+            </div>
+            <button className="bg-zinc-500 p-1 m-1 rounded-full font-bold text-sm text-zinc-50 hover:bg-zinc-800 " onClick={addStorage}>Adicionar</button>    
+          
         </div>
     </div>
   );
 };
 
-export default ProductsApp;
+export default StoragesApp;
